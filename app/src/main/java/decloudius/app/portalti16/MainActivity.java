aa5c83b37e7258427f5b73dbbdfb7f925ca576ee
decloudius <umairbijey@gmail.com>
2018-11-26 18:21:01 +0700

    jalanin requestDaftarMahasiswa

--- app/src/main/java/decloudius/app/portalti16/MainActivity.java
+++ app/src/main/java/decloudius/app/portalti16/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity{
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        requestDaftarMahasiswa();
     }
 
     private void requestDaftarMahasiswa(){
