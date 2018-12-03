2cfe5f25a7a8ae7c35d84666ff818c2b82162b94
decloudius <umairbijey@gmail.com>
2018-12-03 18:04:40 +0700

    add new activity

---
+++ app/src/main/java/decloudius/app/portalti16/AddMahasiswaActivity.java
@@ -0,0 +1,34 @@
+package decloudius.app.portalti16;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AddMahasiswaActivity extends AppCompatActivity {
+
+    private EditText edtName, edtNim;
+    private Button btnAdd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_mahasiswa);
+
+        edtName = (EditText) findViewById(R.id.edt_name);
+        edtNim = (EditText) findViewById(R.id.txt_nim);
+        btnAdd = (Button) findViewById(R.id.btn_add);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNewMahasiswa();
+            }
+        });
+    }
+
+    private void addNewMahasiswa(){
+
+    }
+}
