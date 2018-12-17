0e26bc64bddad6742e2264fce9303197cc87249e
decloudius <umairbijey@gmail.com>
2018-12-17 16:56:13 +0700

    add consts

---
+++ app/src/main/java/decloudius/app/portalti16/util/Consts.java
@@ -0,0 +1,16 @@
+package decloudius.app.portalti16.util;
+
+/**
+ * Created by Umair on 12/17/2018.
+ */
+
+
+public class Consts {
+
+    public static final String KEY_ACTION_DETAIL= "action";
+
+    public static final String INTENT_ADD = "add";
+    public static final String INTENT_EDIT = "edit";
+
+
+}
