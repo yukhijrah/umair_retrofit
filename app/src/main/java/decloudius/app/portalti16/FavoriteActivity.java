a0a02c49859b40bd10ca7bf12b775024f19bf5a9
decloudius <umairbijey@gmail.com>
2019-01-07 18:12:02 +0700

    sudah bisa add favorite

---
+++ app/src/main/java/decloudius/app/portalti16/FavoriteActivity.java
@@ -0,0 +1,13 @@
+package decloudius.app.portalti16;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FavoriteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite);
+    }
+}
